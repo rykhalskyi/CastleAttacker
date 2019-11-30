6d03d2efc37ebd6010ff81a72a3c56b8ae9e217d
rykhalskyi <rikhalskiy@gmail.com>
2019-11-30 22:06:19 +0200

    Add DiceTypes

---
+++ CasstleAtacker/Models/GameRound.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasstleAtacker.Models
+{
+    public enum DiceType
+    {
+        Red,
+        Yellow,
+        Orange,
+        Green,
+        Blue,
+        Violet,
+        Brown,
+        Trade,       //8,9
+        Attack,      //10,11
+        Repairment,  //12
+    }
+
+    class GameRound
+    {
+        static Random _random = new Random();
+
+        static DiceType[] _diceTypes = {DiceType.Red, DiceType.Orange, DiceType.Yellow, DiceType.Green,
+                                DiceType.Blue, DiceType.Violet, DiceType.Brown,
+                                DiceType.Trade, DiceType.Trade, 
+                                DiceType.Attack, DiceType.Attack,
+                                DiceType.Repairment};
+
+        public static (DiceType, DiceType) Run()
+        {
+            return (_diceTypes[_random.Next(12)], _diceTypes[_random.Next(12)]);
+        }
+
+    }
+}
---
+++ CasstleAtacker/Views/DicesUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CasstleAtacker.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CasstleAtacker.Views
+{
+    public sealed partial class DicesUserControl : UserControl
+    {
+        public DicesUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public DiceType FirstDice
+        {
+            get { return (DiceType)GetValue(FirstDiceProperty); }
+            set { SetValue(FirstDiceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FirstDice.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FirstDiceProperty =
+            DependencyProperty.Register("FirstDice", typeof(DiceType), typeof(DicesUserControl), new PropertyMetadata(0, OnFirstDiceSet));
+
+        private static void OnFirstDiceSet(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DiceType SecondDice
+        {
+            get { return (DiceType)GetValue(SecondDiceProperty); }
+            set { SetValue(SecondDiceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SecondDice.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SecondDiceProperty =
+            DependencyProperty.Register("SecondDice", typeof(DiceType), typeof(DicesUserControl), new PropertyMetadata(0, OnSecondDiceSet));
+
+        private static void OnSecondDiceSet(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
